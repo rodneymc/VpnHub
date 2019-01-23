3341295ee9689cf1772743ab94dd623c923297f9
rodney <rodney@daftdroid.com>
2019-01-23 09:45:48 +0000

    Added some test code for self signed certificates

--- src/main/java/com/daftdroid/vpnhub/openvpn/Test.java
+++ src/main/java/com/daftdroid/vpnhub/openvpn/Test.java
@@ -1,9 +1,19 @@
 package com.daftdroid.vpnhub.openvpn;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+
 import com.daftdroid.vpnhub.openvpn.options.CommonOptions;
 
 public class Test {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws Exception {
         System.out.println(new CommonOptions("dafttun").toString());
+        
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(4096);
+        KeyPair key = keyGen.generateKeyPair();
+        
+        
+        System.out.println(OvpnTls.selfSign(key, "CN=demo.example.com"));
     }
 }
