7da025ced70608f12d19851d7301e85723b3b2c6
rodney <rodney@daftdroid.com>
2019-02-05 09:49:56 +0000

    work in progress, added reading back of private key

--- src/main/java/com/daftdroid/vpnhub/openvpn/OvpnTls.java
+++ src/main/java/com/daftdroid/vpnhub/openvpn/OvpnTls.java
@@ -1,25 +1,51 @@
 package com.daftdroid.vpnhub.openvpn;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.math.BigInteger;
+import java.security.KeyFactory;
 import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Security;
 import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 import java.util.Calendar;
 import java.util.Date;
 
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.x500.X500Name;
 import org.bouncycastle.asn1.x509.BasicConstraints;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.CertIOException;
 import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.X509v3CertificateBuilder;
 import org.bouncycastle.cert.jcajce.JcaX509v3CertificateBuilder;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.ContentSigner;
 import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
+import org.bouncycastle.util.io.pem.PemObject;
+import org.bouncycastle.util.io.pem.PemObjectParser;
+import org.bouncycastle.util.io.pem.PemReader;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.bouncycastle.openssl.jcajce.JcaPEMWriter;
 
+
 public class OvpnTls {
+
+    
+    static {
+        java.security.Security.addProvider(
+                new org.bouncycastle.jce.provider.BouncyCastleProvider()
+       );
+    }
 
     private final OvpnTls parent;
     
@@ -27,57 +53,151 @@ public class OvpnTls {
         this.parent = parent;
     }
     
-    private X509v3CertificateBuilder x;
-    
+    private X509CertificateHolder certificateHolder;
+    private KeyPair keyPair;
+    private String commonName;
+
     public String getCertPem() {
-        return null;
+        try {
+            StringWriter s = new StringWriter();
+            JcaPEMWriter p = new JcaPEMWriter(s);
+            p.writeObject(certificateHolder);
+            p.close();
+            return s.toString();
+        } catch (IOException e) {
+            // None of the input/output to this method are files, network connections or whatnot,
+            // an IOException simply shouldn't occur???
+            
+            throw new RuntimeException("Unexpected IOException", e); // TODO improve me?
+        }
     }
 
     public String getKeyPem() {
-        return null;
+        try {
+            StringWriter s = new StringWriter();
+            JcaPEMWriter p = new JcaPEMWriter(s);
+            p.writeObject(keyPair);
+            p.close();
+            return s.toString();
+        } catch (IOException e) {
+            // None of the input/output to this method are files, network connections or whatnot,
+            // an IOException simply shouldn't occur???
+            
+            throw new RuntimeException("Unexpected IOException", e); // TODO improve me?
+        }
+    }
+    // TODO password
+    public void loadData(String data) {
+        
+        PemObject ob;
+        PemObjectParser parser;
+        
+        try (PemReader reader = new PemReader(new StringReader(data));) {
+            while ((ob = reader.readPemObject()) != null) {
+                System.out.println(ob.toString());
+             
+                String type = ob.getType();
+                byte databytes[] = ob.getContent();
+                KeyFactory kf;
+                if ("RSA PRIVATE KEY".equals(type)) {
+                    try {
+                        kf = KeyFactory.getInstance("RSA"); }
+                    catch (NoSuchAlgorithmException e) {
+                        throw new RuntimeException("RSA Not supported????", e); //TODO
+                    }
+                    
+                    PrivateKey k;
+                    PublicKey k2;
+                    try {
+                        
+                        PKCS8EncodedKeySpec keyspec = new PKCS8EncodedKeySpec(databytes);
+                        k = kf.generatePrivate(keyspec);
+                        //k2 = converter.getPublicKey(pub);
+                        keyPair = new KeyPair(null, k);
+                    } catch (InvalidKeySpecException e) {
+                        System.out.println("Yikes");// TODO
+                        e.printStackTrace();
+                    }
+                } else if ("CERTIFICATE".equals(type)) {
+                    
+                }
+                
+                //KeyPair k = new KeyPair(ob.getContent());
+            }
+        } catch (IOException e) {
+            // No IO involved????
+            throw new RuntimeException("Unexpected IOException", e);
+        }
     }
 
+    /*
+     * Gets the parent node.
+     */
+    public OvpnTls getParent() {
+        return parent;
+    }
+    /*
+     * Walks back through the certificate chain to the root, (returns this if this is the root)
+     */
     public OvpnTls getCa() {
-        return null;
+        OvpnTls nextParent = this;
+        
+        while (nextParent.parent != null) {
+            nextParent = nextParent.parent;
+        }
+        
+        return nextParent;
     }
     
 
-    public static String selfSign(KeyPair keyPair, String subjectDN) throws OperatorCreationException, CertificateException, IOException
+    public static OvpnTls generateCA (String commonName, int years)
     {
-//        Provider bcProvider = new BouncyCastleProvider();
-//        Security.addProvider(bcProvider);
+        try {
+            long now = System.currentTimeMillis();
+            Date startDate = new Date(now);
+            String subjectDN = "CN="+commonName;
+    
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(4096);
+            KeyPair keyPair = keyGen.generateKeyPair();
+            
+            X500Name dnName = new X500Name(subjectDN);
+            BigInteger certSerialNumber = new BigInteger(Long.toString(now)); // <-- Using the current timestamp as the certificate serial number
+    
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(startDate);
+            calendar.add(Calendar.YEAR, years);
+    
+            Date endDate = calendar.getTime();
+    
+            JcaX509v3CertificateBuilder certBuilder = new JcaX509v3CertificateBuilder(dnName, certSerialNumber, startDate, endDate, dnName, keyPair.getPublic());
+    
+            // Extensions --------------------------
+    
+            // Basic Constraints
+            BasicConstraints basicConstraints = new BasicConstraints(true); // <-- true for CA, false for EndEntity
+    
+            certBuilder.addExtension(new ASN1ObjectIdentifier("2.5.29.19"), true, basicConstraints); // Basic Constraints is usually marked as critical.
+    
+            ContentSigner signer = new JcaContentSignerBuilder("SHA256withRSA")
+                    .build(keyPair.getPrivate());
+            
+            X509CertificateHolder h = certBuilder.build(signer);
+            
+            
+            //StringWriter s = new StringWriter();
+            //JcaPEMWriter p = new JcaPEMWriter(s);
+            //p.writeObject(h);
+            //p.close();
+            
+            OvpnTls ovpnTls = new OvpnTls(null); // null = no parent
+            ovpnTls.certificateHolder = h;
+            ovpnTls.keyPair = keyPair;
+            return ovpnTls;
+        } catch (NoSuchAlgorithmException | OperatorCreationException  | CertIOException e) {
 
-        long now = System.currentTimeMillis();
-        Date startDate = new Date(now);
-
-        X500Name dnName = new X500Name(subjectDN);
-        BigInteger certSerialNumber = new BigInteger(Long.toString(now)); // <-- Using the current timestamp as the certificate serial number
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(startDate);
-        calendar.add(Calendar.YEAR, 1); // <-- 1 Yr validity
-
-        Date endDate = calendar.getTime();
-
-
-        JcaX509v3CertificateBuilder certBuilder = new JcaX509v3CertificateBuilder(dnName, certSerialNumber, startDate, endDate, dnName, keyPair.getPublic());
-
-        // Extensions --------------------------
-
-        // Basic Constraints
-        BasicConstraints basicConstraints = new BasicConstraints(true); // <-- true for CA, false for EndEntity
-
-        certBuilder.addExtension(new ASN1ObjectIdentifier("2.5.29.19"), true, basicConstraints); // Basic Constraints is usually marked as critical.
-
-        ContentSigner signer = new JcaContentSignerBuilder("SHA256withRSA")
-                .build(keyPair.getPrivate());
-        
-        X509CertificateHolder h = certBuilder.build(signer);
-        
-        StringWriter s = new StringWriter();
-        JcaPEMWriter p = new JcaPEMWriter(s);
-        p.writeObject(h);
-        p.close();
-        return s.toString();
+            // TODO some sort of "this code can't create cert on tis platform" status.
+            return null;
+        }
     }
 }
--- src/main/java/com/daftdroid/vpnhub/openvpn/Test.java
+++ src/main/java/com/daftdroid/vpnhub/openvpn/Test.java
@@ -1,5 +1,8 @@
 package com.daftdroid.vpnhub.openvpn;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 
@@ -11,16 +14,14 @@ public class Test {
     public static void main(String args[]) throws Exception {
         System.out.println(new CommonOptions("dafttun").toString());
         
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
-        keyGen.initialize(4096);
-        KeyPair key = keyGen.generateKeyPair();
+        OvpnTls t = new OvpnTls(null);
+        String data = new String(Files.readAllBytes(Paths.get("testdata")));
+        t.loadData(data);
+        System.out.println(t.getKeyPem());
         
-        String s = OvpnTls.selfSign(key, "CN=demo.example.com");
-        System.out.println(s);
-        
-        //Certificate c = OvpnTls.selfSign(key, "CN=demo.example.com");
-        //System.out.println(c.getEncoded().toString());
-        //OvpnTls tls = new OvpnTls(null);
+        //OvpnTls tls = OvpnTls.generateCA("testing.example.com", 5);
+        //System.out.println(tls.getKeyPem());
+        //System.out.println(tls.getCertPem());
         //tls.getCertPem();
 
     }
